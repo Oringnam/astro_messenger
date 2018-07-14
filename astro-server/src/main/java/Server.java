c000d73e340198ba2af9359be3d1060ed7e4cda4
Oringnam <oringnam@gmail.com>
2018-07-14 17:31:49 +0900

    * test

--- astro-server/src/main/java/Server.java
+++ astro-server/src/main/java/Server.java
@@ -10,6 +10,7 @@ import java.util.concurrent.atomic.AtomicBoolean;
 import message.AstroMessage;
 import monitor.AstroMonitor;
 import network.RMI;
+import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import utils.AstroProperties;
