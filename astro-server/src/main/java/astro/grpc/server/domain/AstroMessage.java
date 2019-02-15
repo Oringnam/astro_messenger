2a1a5c416c41fc482faa54b60efa3eadc29654a9
Hee-Don <don210198@gmail.com>
2019-02-15 15:27:13 +0900

    # astro sever mybatis 적용 - AstroMessage domain 추가 - Mapper, config 설정

---
+++ astro-server/src/main/java/astro/grpc/server/domain/AstroMessage.java
@@ -0,0 +1,9 @@
+package astro.grpc.server.domain;
+
+public class AstroMessage {
+    private String topic;
+    private int index;
+    private String datetime;
+    private String uuid;
+    private String message;
+}
